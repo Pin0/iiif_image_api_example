a5b35aa93ca660ef483ec3f54e2fb89929dbd640
Martien Nijdam <Martien.Nijdam@vitecsoftware.com>
2025-06-26 10:30:09 +0200

    to new design

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
-//import { RouterLink, RouterView } from 'vue-router'
 import IiifImage from './components/IiifImage.vue'
-
 </script>
 
 <template>
-    <div class="wrapper">
-      <IiifImage />
-    </div>
-
+  <div class="wrapper">
+    <IiifImage />
+  </div>
 </template>
 
 <style scoped>
+.wrapper {
+  width: 100%;
+  min-height: 100vh;
+  background-color: var(--color-background);
+}
 </style>
